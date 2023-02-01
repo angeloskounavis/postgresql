e84cdd5f6519c97511678e519212f572605a502c
Angelos <kounavisangelo@gmail.com>
2023-02-01 17:52:38 -0500

    lab 33

---
+++ README.md
@@ -0,0 +1,31 @@
+# Lab 32 and Lab 33
+## Author: Angelos Kounavis
+
+### How to run tests:
+
+python manage.py runserver Navigate to snacks/tests 
+Run in terminal: python manage.py test
+
+Setup
+> python3.11 -m venv .venv 
+> pip install django 
+> pip install djangorestframework
+> pip install psycopg2-binary
+> django-admin startproject jobs_project . 
+> python manage.py startapp jobs
+> pip install djangorestframework-simplejwt
+> pip install gunicorn
+> pip install whitenoise
+> python manage.py migrate
+> pip freeze > requirements.txt
+> python manage.py createsuperuser
+
+To run:
+
+> gunicorn jobs_project.wsgi:application --bind 0.0.0.0:8000 --workers 4
+
+Load the site at http://0.0.0.0:8001/
+
+
+- Aubrey 
+- Ricardo
--- jobs_project/settings.py
+++ jobs_project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -85,6 +86,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "postgres",
+#         "USER": "postgres",
+#         "PASSWORD": "postgres",
+#         "HOST": "db",
+#         "PORT": 5432,
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -132,5 +143,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
